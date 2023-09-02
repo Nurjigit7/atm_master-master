b1a40cdc1a00914b59f515085029942890f07511
626 <nurjigitumarov22@gmail.com>
2023-09-02 19:46:37 +0300

    Initial commit

--- atm_master-master/src/atm/service/impl/AccountServiceImpl.java
+++ atm_master-master/src/atm/service/impl/AccountServiceImpl.java
@@ -3,8 +3,7 @@ package atm.service.impl;
 import atm.dao.AccountDao;
 import atm.model.UserAccount;
 import atm.service.AccountService;
-
-import java.util.InputMismatchException;
+import enums.Services;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -37,29 +36,26 @@ public class AccountServiceImpl implements AccountService {
             UserAccount user = accountDao.getUserAccounts()
                     .stream().filter(n -> n.getName().equalsIgnoreCase(name) && n.getLastName()
                             .equalsIgnoreCase(lastName)).findFirst().orElseThrow(() -> new RuntimeException("Not found User"));
-            System.out.println("Test " + user);
             int choice = 0;
 //            for (int i = 0; i < accountDao.getUserAccounts().size(); i++) {
 //                UserAccount user = accountDao.getUserAccounts().get(i);
 //                if (user.getName().equalsIgnoreCase(name) && user.getLastName().equalsIgnoreCase(lastName)) {
-            System.out.println("Выборите услуги :");
+            System.out.println("select services !!");
             String word;
-            System.out.print(" Для пополнения баланса нажмите на [1] "
-                    + "\n" + " Для получение депозита нажните на   [2] "
-                    + "\n" + " Для отправки денег другу нажните на  [3] "
-                    + "\n" + " Для снятия наличных денег нажните на [4]");
+            System.out.print(Services.BALANCE + " Для пополнения баланса нажмите на [1] "
+                    + "\n" + Services.DEPOSIT + " Для получение депозита нажните на   [2] "
+                    + "\n" + Services.SEND_MONEY_TO_FRIEND + " Для отправки денег другу нажните на  [3] "
+                    + "\n" + Services.WITHDRAW_MONEY + " Для снятия наличных денег нажните на [4]");
 
             while (true) {
-
                 word = scanner.nextLine();
                 if ("1234".contains(word) && !word.equals("")) {
-                    choice= Integer.parseInt(word);
+                    choice = Integer.parseInt(word);
                     break;
-                } else if (!word.equals("")) {
-                    System.out.println("Выборите услугу !!!");
+                } else if (!word.equals(" ")) {
+                    System.out.println("select services !!!");
                 }
             }
-
             switch (choice) {
                 case 1 -> {
                     System.out.println("Введите номер карта ->");
@@ -93,7 +89,6 @@ public class AccountServiceImpl implements AccountService {
             System.out.println("\n Error:" + e);
 
         }
-
     }
 
     @Override
@@ -118,7 +113,7 @@ public class AccountServiceImpl implements AccountService {
             int sum = scanner.nextInt();
             user.setBalance(sum);
             accountDao.getUserAccounts().add(user);
-            System.out.println("Суума пополнина" + user);
+            System.out.println("Deposit amount" + user);
         } else {
             System.out.println("Не правилный данный !!!");
         }
@@ -126,16 +121,11 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public void sendToFriend(String friendCardNumber) {
-//        accountDao.getUserAccounts().stream()
-//                .filter(n -> n.getCardNumber().equals(friendCardNumber)).
-//                findFirst().ifPresentOrElse(user -> user.
-//                        setBalance(user.getBalance() + money), () -> System.out.println("Друг не найден!!"));
-        System.out.println("Введите номер карты !!");
+        System.out.println("Введите номер карты :");
         String cardNumber = scanner.nextLine();
-
-        System.out.println("Введите пин код!!");
+        System.out.println("Введите cвои пин код :");
         String pinCode = scanner.nextLine();
-        System.out.println("Введите сколько хотите денег перевести !!!");
+        System.out.println("Сколько хотите денег перевести ??");
         int money = scanner.nextInt();
         for (int i = 0; i < accountDao.getUserAccounts().size(); i++) {
             UserAccount user = accountDao.getUserAccounts().get(i);
@@ -151,22 +141,39 @@ public class AccountServiceImpl implements AccountService {
                 user.setBalance(user.getBalance() + money);
                 accountDao.getUserAccounts().add(user);
                 System.out.println("Ваше данных : " + user);
-                System.out.println("данный друга : " + user);
+                System.out.println("Данный друга : " + user);
             } else {
-                System.out.println("Друг не найден!!");
-            }
-
+                System.out.println("Логин или пароль неверный");
+    }
+//        System.out.println("Введите номер карты :");
+//        String cardNumber = scanner.nextLine();
+//        System.out.println("Введите свой  пин код :");
+//        String pinCode = scanner.nextLine();
+//        System.out.println("Сколько денег хотите перевести?");
+//        int amount = scanner.nextInt();
+//        for (int i = 0; i < accountDao.getUserAccounts().size(); i++) {
+//            UserAccount user = accountDao.getUserAccounts().get(i);
+//            if (user.getCardNumber().equals(cardNumber) && user.getPinCode().equals(pinCode)) {
+//                user.setBalance(user.getBalance()-amount);
+//                user.setBalance(user.getBalance()+amount);
+//                accountDao.getUserAccounts().add(user);
+//                System.out.println(" Ваши данные :  " + user);
+//                System.out.println("Данные друга  : " + user);
+//            } else {
+//                System.out.println("Логин или пароль неверный");
+//            }
         }
     }
 
     @Override
     public void withdrawMoney(int amount) {
+        System.out.println("");
         int[] money = {1000, 500, 200, 100, 50};
         int choice = amount;
         for (int moneys : money) {
             int arifmet = amount / moneys;
             if (arifmet > 0) {
-                System.out.println(moneys + "[ = ]" + arifmet);
+                System.out.println(moneys + "-> штук :" + arifmet);
                 choice %= moneys;
             }
         }
---
+++ atm_master-master/src/enums/Services.java
@@ -0,0 +1,12 @@
+package enums;
+
+public enum Services {
+    BALANCE,
+    DEPOSIT,
+    SEND_MONEY_TO_FRIEND,
+    WITHDRAW_MONEY
+
+
+
+
+}
