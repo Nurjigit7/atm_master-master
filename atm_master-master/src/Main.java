1e72525dc00996db8619e4b2ec20c50643b66f30
626 <nurjigitumarov22@gmail.com>
2023-09-05 18:04:23 +0300

    successfully

--- atm_master-master/src/Main.java
+++ atm_master-master/src/Main.java
@@ -3,12 +3,12 @@ import atm.service.impl.AccountServiceImpl;
 public class Main {
     public static void main(String[] args) {
         AccountServiceImpl accountService = new AccountServiceImpl();
-        accountService.singUp("Nurjigit", "Umarov");
+        accountService.singUp("Nurjigit","Umarov");
         accountService.singUp("Rysbek", "Sharapov");
 
 
         while (true) {
-            accountService.singIn("Nurjigit", "Umarov");
+            accountService.singIn("Nurjigit","Umarov");
         }
 
 
